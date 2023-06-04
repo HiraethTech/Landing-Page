216cc464d13a76162b0ce75f3d60d0e0e78c6c79
MichaelHanDev <135553104+MichaelHanDev@users.noreply.github.com>
2023-06-04 17:20:39 -0500

    Update app.js
    Website now creates an alert when url for the entire website is copied.

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ async function copyText(e) {
 
 shareButtons.forEach(shareButton => 
     shareButton.addEventListener('click', copyText))
+
+const shareButton = document.querySelectorAll('.share-button')
+console.log(shareButton)
+
+async function copyText(e) {
+//prevent button from going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("URL Copied:" + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButton.forEach(shareButton => 
+    shareButton.addEventListener('click', copyText))
